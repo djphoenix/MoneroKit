dc2b716cea75b75e6a7ace98b4d320917b027534
Yury Popov <github@phoenix.dj>
2018-01-10 23:54:22 +0300

    Fix import style

--- MoneroKitSample/iOS/AppDelegate.h
+++ MoneroKitSample/iOS/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Yury Popov on 10.01.2018.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- MoneroKitSample/iOS/main.m
+++ MoneroKitSample/iOS/main.m
@@ -5,7 +5,7 @@
 //  Created by Yury Popov on 10.01.2018.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
